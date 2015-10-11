df02403df6bfed6a6909997aae409865ef58c499
Charlotte Chang <yellowspiderlove@gmail.com>
2015-10-11 17:35:21 -0400

    refactor

--- grains.rb
+++ grains.rb
@@ -1,20 +1,12 @@
 class Grains
-	def self.square(number)
-		square_grains = 
-		{
-			1 => 1,
-			2 => 2,
-			3 => 4,
-			4 => 8,
-			16 => 32_768,
-			32 => 2_147_483_648,
-			64 => 9_223_372_036_854_775_808
-		}
+	
+	TOTAL_SQUARES = 64 
 
-		square_grains.fetch(number)
+	def self.square(number)
+		2 ** ( number - 1 )
 	end
 
 	def self.total
-		18_446_744_073_709_551_615
+		(1..TOTAL_SQUARES).inject {|sum,n| sum + square(n)}
 	end
 end
